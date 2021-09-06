dc7e74c7393afa0a99fe5a7e01048bb8668259e5
Fr1nge333 <37176227+fr1nge333@users.noreply.github.com>
2021-09-06 17:54:36 +0800

    分类

--- src/main/java/com/fr1nge/myblog/service/BlogCategoryService.java
+++ src/main/java/com/fr1nge/myblog/service/BlogCategoryService.java
@@ -1,5 +1,8 @@
 package com.fr1nge.myblog.service;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.fr1nge.myblog.entity.Blog;
 import com.fr1nge.myblog.entity.BlogCategory;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -12,5 +15,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2021-08-31
  */
 public interface BlogCategoryService extends IService<BlogCategory> {
-
+    IPage<BlogCategory> selectPage(IPage<BlogCategory> page, Wrapper<BlogCategory> wrapper);
 }
