44ec95e23f8e079e2009ce100547a2258706788c
Fr1nge333 <37176227+fr1nge333@users.noreply.github.com>
2021-09-07 17:43:26 +0800

    修改

--- src/main/java/com/fr1nge/myblog/config/intercepors/LoginInterceptor.java
+++ src/main/java/com/fr1nge/myblog/config/intercepors/LoginInterceptor.java
@@ -31,23 +31,25 @@ public class LoginInterceptor implements HandlerInterceptor {
         //这里的token是登陆时放入session的
         String token = JwtUtil.getSubject(request, tokenName, signingKey);
 
-        log.debug("请求路径：" + request.getRequestURI());
+        log.info("请求路径：" + request.getRequestURI());
 
         //如果session中没有user，表示没登陆
         if (token == null) {
             //这个方法返回false表示忽略当前请求，如果一个用户调用了需要登陆才能使用的接口，如果他没有登陆这里会直接忽略掉
             //当然你可以利用response给用户返回一些提示信息，告诉他没登陆
-            log.debug("no session" + ",请求路径：" + request.getRequestURI());
+            log.info("no session" + ",请求路径：" + request.getRequestURI());
             response.sendRedirect("/admin/login");
             return false;
         } else {
+            log.info("has session" + ",请求路径：" + request.getRequestURI());
+            log.info("session=" + session.getAttribute("loginUser"));
+            log.info("create newToken begin");
             //更新token
             String newToken = JwtUtil.generateToken(signingKey, (String) session.getAttribute("loginUser"));
             session.setAttribute(tokenName, newToken);
+            log.info("create newToken end");
 
-            log.debug("has session" + ",请求路径：" + request.getRequestURI());
-            log.debug("session=" + session.getAttribute("loginUser"));
-            log.debug("newToken=" + newToken);
+
 
             return true;    //如果session里有user，表示该用户已经登陆，放行，用户即可继续调用自己需要的接口
         }
@@ -57,13 +59,13 @@ public class LoginInterceptor implements HandlerInterceptor {
     public void postHandle(HttpServletRequest request, HttpServletResponse response,
                            Object handler, @Nullable ModelAndView modelAndView) throws Exception {
 
-        //log.debug ("==========postHandle===========");
+        //log.info ("==========postHandle===========");
     }
 
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
                                 Object handler, @Nullable Exception ex) throws Exception {
 
-        //log.debug ("========afterCompletion========");
+        //log.info ("========afterCompletion========");
     }
 
 }
