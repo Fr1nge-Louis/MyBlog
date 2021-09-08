e0449e56e9ab1692f596b3a2cb0f1b77fbe295c9
Fr1nge333 <37176227+fr1nge333@users.noreply.github.com>
2021-09-08 17:26:46 +0800

    update

---
+++ WebConfig.java
@@ -0,0 +1,38 @@
+package com.fr1nge.myblog.config;
+
+import com.fr1nge.myblog.config.intercepors.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    // 这个方法是用来配置静态资源的，比如html，js，css，等等
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(1, TimeUnit.HOURS).cachePublic());
+
+    }
+
+    // 这个方法用来注册拦截器，我们自己写好的拦截器需要通过这里添加注册才能生效
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // addPathPatterns("/**") 表示拦截所有的请求，
+        // excludePathPatterns("/login", "/register") 表示除了登陆与注册之外，因为登陆注册不需要登陆也可以访问
+
+        String[] exclude = {"/admin/dist/**", "/admin/plugins/**", "/admin/user/login", "/admin/login"};
+
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/admin/**").excludePathPatterns(exclude);
+    }
+}
